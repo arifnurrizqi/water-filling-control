3070687b6f4861b4e8cf3b000dc191ae5a1902f2
Arif Nur Rizqi <arifnurrizqi>
2022-02-22 13:08:37 +0700

    menambahkan file baru

---
+++ multiple_output_websocket.ino
@@ -0,0 +1,169 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Arduino_JSON.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "Home-Asrigendis";
+const char* password = "slotblokir";
+
+AsyncWebServer server(80);
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 0, 100);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 0, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
+
+AsyncWebSocket ws("/ws");
+
+#define NUM_OUTPUTS  3
+
+int outputGPIOs[NUM_OUTPUTS] = {5, 4, 0};
+
+const int button1 = 14;
+const int button2 = 12;
+const int button3 = 13;
+
+void initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+String getOutputStates(){
+  JSONVar myArray;
+  for (int i =0; i<NUM_OUTPUTS; i++){
+    myArray["gpios"][i]["output"] = String(outputGPIOs[i]);
+    myArray["gpios"][i]["state"] = String(digitalRead(outputGPIOs[i]));
+  }
+  String jsonString = JSON.stringify(myArray);
+  return jsonString;
+}
+
+void notifyClients(String state) {
+  ws.textAll(state);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "states") == 0) {
+      notifyClients(getOutputStates());
+    }
+    else{
+      int gpio = atoi((char*)data);
+      digitalWrite(gpio, !digitalRead(gpio));
+      notifyClients(getOutputStates());
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
+
+void setup(){
+  Serial.begin(115200);
+
+  for (int i =0; i<NUM_OUTPUTS; i++){
+    pinMode(outputGPIOs[i], OUTPUT);
+    digitalWrite(outputGPIOs[i], HIGH);
+  }
+  
+  initSPIFFS();
+
+  // Configures static IP address
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+  
+  initWiFi();
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  
+  initWebSocket();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html",false);
+  });
+
+  server.serveStatic("/", SPIFFS,   "/");
+  
+  server.begin();
+}
+
+void loop() {
+  
+  ArduinoOTA.handle();
+  ws.cleanupClients();
+
+}
